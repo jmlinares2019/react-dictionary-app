7ed0d92fd3c3980c96260d0245498d7609030c64
jmlinares2019 <jlinaresmar@gmail.com>
2024-08-13 19:56:24 +0200

    Restructuring NoEntry component and creating its own stylesheet

--- src/components/NoEntry.jsx
+++ src/components/NoEntry.jsx
@@ -1,12 +1,18 @@
 function NoEntry(props){
 
     const notFound = props.result; 
-    // console.log()
+    
     return (
         // <h2>{props.result.message}</h2>
         <div id="no-entry">
-            <p>{notFound.message}</p>
-            <p>{notFound.resolution}</p>
+            <div className="container">
+                <div className="row">
+                    <div className="col-10">
+                        <p className="error-message">{notFound.message}</p>
+                        <p className="error-suggestion">{notFound.resolution}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
